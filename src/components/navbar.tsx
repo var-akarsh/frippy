c155c9234a924c1a5dbd143a4d1d94a15096ebb7
Gautamgarg-at-518075716045 <gautamgarg991@gmail.com>
2024-12-08 12:10:03 +0530

    Navbar redirection and minor chanages

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = () => {
   const menuItems = [
     { name: "Home", href: "#home" },
     { name: "Why Frippy", href: "#why-frippy" },
-    { name: "Testimonials", href: "#testimonials" },
-    { name: "Reviews", href: "#" },
+    { name: "Products", href: "#products" },
+    { name: "Reviews", href: "#testimonials" },
     { name: "FAQ", href: "#faq" },
     { name: "Contact", href: "#contact" },
   ];
